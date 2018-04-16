58123e6319de5d93786c1f001e60580a64bde115
dmshikoff <dmshikoff@gmail.com>
2018-04-16 08:51:53 -0700

    added start script

--- routes/authors.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../server/controller/authors')
-
-router.get('/', controller.getAll)
-router.get('/:id', controller.getOne)
-router.post('/', controller.create)
-
-module.exports = router
